30343298ed22d27e73a7349b6e884ca5a5f378da
mvtistr <matisepu.unab@gmail.com>
2026-05-31 15:07:51 -0400

    inicializando proyecto

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ mensaje: "Backend funcionando" });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
